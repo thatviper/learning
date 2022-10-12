adeaa317a743731a50989ac7f359635ce858646c
thatviper <challanaganarasimhareddy@gmail.com>
2022-10-12 14:58:31 +0530

    resolved all the issues

--- golang/grade-school/grade_school.go
+++ golang/grade-school/grade_school.go
@@ -1,19 +1,21 @@
 package school
 
-import(
-    "sort"
+import (
+	"sort"
 )
+
 // Define the Grade and School types here.
-type Grade struct{
-    class int
-    students []string
+type Grade struct {
+	class    int
+	students []string
 }
-type School struct{
-    maxGrade int 
-    roster map[int]*Grade
+type School struct {
+	maxGrade int
+	roster   map[int]*Grade
 }
+
 func New() *School {
-	return &School{ roster : make(map[int]*Grade)}
+	return &School{roster: make(map[int]*Grade)}
 }
 func (s *School) Add(student string, grade int) {
 	_, ok := s.roster[grade]
@@ -25,9 +27,9 @@ func (s *School) Add(student string, grade int) {
 	} else {
 		s.roster[grade].students = append(s.roster[grade].students, student)
 	}
-} 
+}
 func (s *School) Grade(level int) []string {
-    _, ok := s.roster[level]
+	_, ok := s.roster[level]
 	if !ok {
 		return []string{}
 	}
@@ -36,11 +38,11 @@ func (s *School) Grade(level int) []string {
 func (s *School) Enrollment() []Grade {
 	allStudents := []Grade{}
 	for i := 1; i <= s.maxGrade; i++ {
-        _,ok := s.roster[i]
-        if(ok){
-            sort.Strings(s.roster[i].students)
+		_, ok := s.roster[i]
+		if ok {
+			sort.Strings(s.roster[i].students)
 			allStudents = append(allStudents, *s.roster[i])
-        }
+		}
 	}
 	return allStudents
 }
--- golang/sublist/sublist.go
+++ golang/sublist/sublist.go
@@ -3,12 +3,7 @@ package sublist
 type Relation = string
 
 func checkSubList(ll1 int, ll2 int, l1 []int, l2 []int) bool {
-	if ll1 == ll2 && ll1 == 0 {
-		return true
-	}
 	for i := 0; i < ll2-ll1+1; i++ {
-		// subl2 := l2[i : i+ll1]
-		// fmt.Println(subl2)
 		isSubList := true
 		for j := i; j < i+ll1; j++ {
 			if l1[j-i] != l2[j] {
@@ -16,12 +11,6 @@ func checkSubList(ll1 int, ll2 int, l1 []int, l2 []int) bool {
 				continue
 			}
 		}
-		// for idx, val := range subl2 {
-		// 	if l1[idx] != val {
-		// 		isSubList = false
-		// 		continue
-		// 	}
-		// }
 		if isSubList {
 			return isSubList
 		}
@@ -31,7 +20,9 @@ func checkSubList(ll1 int, ll2 int, l1 []int, l2 []int) bool {
 
 func Sublist(l1, l2 []int) string {
 	ll1, ll2 := len(l1), len(l2)
-	// fmt.Println(ll1, ll2)
+	if ll1 == ll2 && ll1 == 0 {
+		return "equal"
+	}
 	if ll1 != ll2 {
 		if ll1 < ll2 && checkSubList(ll1, ll2, l1, l2) {
 			return "sublist"
