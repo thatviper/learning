0be85529969afa193ede322c5f478350a3eb8e0d
thatviper <challanaganarasimhareddy@gmail.com>
2022-10-13 20:42:50 +0530

    error-handling completed

--- golang/error-handling/error_handling.go
+++ golang/error-handling/error_handling.go
@@ -1,1 +1,37 @@
 package erratum
+
+
+func Use(opener ResourceOpener, input string) (err error) {
+	var res Resource
+	flag := false
+	for {
+		res, err = opener()
+		switch err.(type) {
+		case TransientError:
+		default:
+			if err != nil {
+				return err
+			} else {
+                defer res.Close()
+				flag = true
+			}
+		}
+		if flag {
+			break
+		}
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			switch r.(type) {
+			case FrobError:
+				frobErr := r.(FrobError)
+				res.Defrob(frobErr.defrobTag)
+				err = frobErr
+			default:
+				err = r.(error)
+			}
+		}
+	}()
+	res.Frob(input)
+	return nil
+}
